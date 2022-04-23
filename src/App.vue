35490bd2f31be4c561d09246ca67f40b0d2547a6
Kolawole Ezekiel <ezekiel.kolawole@cudium.com>
2022-04-23 13:12:41 +0100

    apllication structure

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
+@import "~@/assets/style.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PageHome from './components/PageHome'
+import PageThreadShow from './components/PageThreadShow'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: PageHome, name: 'Home' },
+  { path: '/thread/:id', component: PageThreadShow, name: 'ThreadShow' }
+]
+
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes // short for `routes: routes`
+})
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
